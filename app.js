0d8e52b9eeeabd3b977a3d70ff6f25825934db1a
Afeez-Ola <afeezbolajiola@gmail.com>
2020-03-13 14:06:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
+var mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session')
+const passport = require('passport')
+    //Set up default mongoose connection
+
+const app = express();
+require('./config/passport')(passport)
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Mongodb connected...'))
+    .catch(err => console.log(err))
+
+app.use(expressLayouts);
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+});
+
+app.use('/', require('./routes/index'));
+
+app.use('/users', require('./routes/users'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
