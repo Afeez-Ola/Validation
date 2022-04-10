8fc7d25145e57d461176347677643dbd8fe1afe6
Afeez Bolaji <afeezbolajiola@gmail.com>
2022-04-10 23:56:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session')
 const passport = require('passport')
