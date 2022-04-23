0ef16a7c91620ebce5356bacc7a8091869c01a88
Afeez Bolaji <afeezbolajiola@gmail.com>
2022-04-23 23:01:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index'));
 
 app.use('/users', require('./routes/users'));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
